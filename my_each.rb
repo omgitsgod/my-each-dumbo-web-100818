3a0c998fe69a9cd2ea3dc314fbcf29a22210ce3a
github username <github email address>
2018-09-21 17:01:17 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < arr.length
+      yield arr[i]
+      i += 1
+    end
+  else "NOPES"
+  end
+  arr
 end
